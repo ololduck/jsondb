38ab61afe365aa3548052c58fd9d0ceaea8f325b
Paul Ollivier <contact@paulollivier.fr>
2014-01-12 12:36:41 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from jsondb import __version__
+
+setup(name='jsondb',
+      version=__version__,
+      description='JSON db engine',
+      author='Paul Ollivier',
+      author_email='contact@paulollivier.fr',
+      url='https://github.com/paulollivier/jsondb',
+      packages=['jsondb'],
+     )
+
