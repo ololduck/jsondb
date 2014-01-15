7fd12358c3cd073aa17e177af2b4a83514a10600
Paul Ollivier <paul.ollivier@ullink.com>
2014-01-15 16:42:53 +0100

    WIP. We have a good steel scaffold for relational db.

--- jsondb/models.py
+++ jsondb/models.py
@@ -2,14 +2,20 @@
 
 import os
 import json
+import random
+import sys
 
 from jsondb import logger
 from jsondb import utils
+
 class JSONdb(object):
     class Filter(object):
         def __init__(self, field, expected):
             self.field = field
             self.expected = expected
+
+        def __repr__(self):
+            return '<Filter: {0}=={1}>'.format(self.field, self.expected)
 
         def validate(self, obj):
             if(hasattr(obj, self.field)):
@@ -48,14 +54,68 @@ class JSONdb(object):
         for key, value in self.tables.items():
             self.save_table(key.replace('.', os.path.sep), value)
 
+    def _check_if_obj_has_not_primitive_fields(self, obj):
+        '''
+        >>> class C(object):
+        ...     def __init__(self, arg):
+        ...         self.arg = arg
+        ...     def __repr__(self):
+        ...         return '<C: {0}>'.format(self.arg)
+        ...
+        >>> c = C("hello")
+        >>> d = C(c)
+        >>> db = JSONdb('test_db')
+        >>> db._check_if_obj_has_not_primitive_fields(c)
+        []
+        >>> db._check_if_obj_has_not_primitive_fields(d)
+        ['arg']
+        '''
+        outcasts = []
+        for key, value in obj.__dict__.items():
+            if(type(value) not in (int, float, bool, dict, list, str)):
+                # the it is not a primitive type
+                outcasts.append(key)
+        return outcasts
 
     def add(self, obj):
+        logger.debug("adding object {0}".format(obj.__dict__))
         classpath = utils.get_classpath(obj)
+        outcasts = self._check_if_obj_has_not_primitive_fields(obj)
+        for outcast in outcasts:
+            # we need to add an id for this field, and put it on the corresponding object.
+            r = random.randint(0, sys.maxint)
+            obj.__dict__[outcast].__dict__['jsondb_id'] = r
+            self.add(obj.__dict__[outcast])
+            obj.__dict__[outcast] = "<jsondb_id:({0};{1})>".format(utils.get_classpath(obj.__dict__[outcast]).replace(os.path.sep, '.'), r)
+
         if(classpath.replace(os.path.sep, '.') not in self.tables):
             self.tables.update({classpath.replace(os.path.sep, '.'): []})
         self.tables[classpath.replace(os.path.sep, '.')].append(obj.__dict__)
         logger.debug(self.tables)
         self.save_table(utils.get_table_name(self.dbpath, obj))
+
+    def _get_relational_fields(self, obj):
+        '''
+        >>> class C(object):
+        ...     def __init__(self, arg):
+        ...         self.arg = arg
+        ...     def __repr__(self):
+        ...         return '<C: {0}>'.format(self.arg)
+        ...
+        >>> c = C("<jsondb_id:(test.path.C;146546)>")
+        >>> d = C(c)
+        >>> db = JSONdb('test_db')
+        >>> db._get_relational_fields(c)
+        ['arg']
+        >>> db._get_relational_fields(d)
+        []
+        '''
+        fields = []
+        for key, value in obj.__dict__.items():
+            if(type(value) == str and value.startswith("<jsondb_id:(")):
+                # then it is another object, and we need to load it
+                fields.append(key)
+        return fields
 
     def get(self, clazz, **kwargs):
         # Here, we should use the visitor pattern, maybe?
@@ -76,10 +136,18 @@ class JSONdb(object):
                     return False
             return True
         for d in data:
+            if('jsondb_id' in d):
+                del d['jsondb_id']
             obj = clazz()
             obj.__dict__.update(d)
             if(is_valid(obj)):
+                for field in self._get_relational_fields(obj):
+                    # load the object's class, and get it
+                    pass
                 objects.append(obj)
         return objects
+
+    def relget(self, clazz, **kwargs):
+        return []
 
 
---
+++ tests/test_relational_db.py
@@ -0,0 +1,36 @@
+import unittest
+import shutil
+import json
+import jsondb
+
+class C(object):
+    def __init__(self, arg=None):
+        self.arg = arg
+
+class A(object):
+    def __init__(self, arg=None):
+        self.arg = arg
+
+class RelationalDBTest(unittest.TestCase):
+    def setUp(self):
+        self.db_path = 'test_db'
+        self.db = jsondb.JSONdb(self.db_path)
+
+    def tearDown(self):
+        shutil.rmtree(self.db_path)
+
+    def test_relational_model_save(self):
+        a = A("Hello")
+        c = C(a)
+        self.db.add(c)
+        got_a = self.db.get(A, arg="Hello")
+        self.assertNotEqual(got_a, [], "A has not been saved correctly, althrough c.arg == a\n{0}".format(self.db.__dict__))
+
+    def test_relational_model_load(self):
+        self.test_relational_model_save()
+        got_c = self.db.relget(C)
+        self.assertNotEqual(got_c, [])
+        c = got_c[0]
+        self.assertNotEqual(c.arg, None)
+        self.assertEqual(c.arg.__class__, A)
+        self.assertEqual(c.arg.arg, "Hello")
